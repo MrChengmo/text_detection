ca5b77c800f1c4b85bf8801a872eaaca0378863c
MrChengmo <cmchengmo@163.com>
2017-06-30 15:49:08 +0800

    删除了部分注释

--- textDetection/textDetection.py
+++ textDetection/textDetection.py
@@ -3,32 +3,26 @@ import sys
 
 import cv2
 import numpy as np
-#添加注释用于测试
+
 
 def preprocess(gray):
-    # 1. Sobel���ӣ�x�������ݶ�
     sobel = cv2.Sobel(gray, cv2.CV_8U, 1, 0, ksize = 3)
-    # 2. ��ֵ��
+
     ret, binary = cv2.threshold(sobel, 0, 255, cv2.THRESH_OTSU+cv2.THRESH_BINARY)
 
-    # 3. ���ͺ͸�ʴ�����ĺ˺���
+
     element1 = cv2.getStructuringElement(cv2.MORPH_RECT, (25, 9))
     element2 = cv2.getStructuringElement(cv2.MORPH_RECT, (20, 6))
     element3 = cv2.getStructuringElement(cv2.MORPH_RECT, (15, 5))
 
-    # 4. ����һ�Σ�������ͻ��
     dilation = cv2.dilate(binary, element2, iterations = 1)
 
-    # 5. ��ʴһ�Σ�ȥ��ϸ�ڣ������ߵȡ�ע������ȥ�������ֱ����
     erosion = cv2.erode(dilation, element1, iterations = 1)
 
-    # 6. �ٴ����ͣ�����������һЩ
     dilation2 = cv2.dilate(erosion, element2, iterations = 3)
 
-    # 6.5 �ٸ�ʴһ�Σ�ȥ�����ͨ��
     erosion2 = cv2.erode(dilation2,element3,iterations = 3)
 
-    # 7. �洢�м�ͼƬ 
     cv2.imwrite("binary.png", binary)
  
     cv2.imwrite("dilation.png" , dilation)
@@ -42,37 +36,37 @@ def preprocess(gray):
 def findTextRegion(img):
     region = []
 
-    # 1. ��������
+
     contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    # 2. ɸѡ��Щ���С��
+
     for i in range(len(contours)):
         cnt = contours[i]
-        # ��������������
+
         area = cv2.contourArea(cnt) 
 
-        # ���С�Ķ�ɸѡ��
+
         if(area < 1000):
             continue
 
-        # �������ƣ����ú�С
+
         epsilon = 0.01 * cv2.arcLength(cnt, True)
         approx = cv2.approxPolyDP(cnt, epsilon, True)
 
-        # �ҵ���С�ľ��Σ��þ��ο����з���
+
         rect = cv2.minAreaRect(cnt)
         print "rect is: "
         print rect
 
-        # box���ĸ��������
+
         box = cv2.cv.BoxPoints(rect)
         box = np.int0(box)
 
-        # ����ߺͿ�
+
         height = abs(box[0][1] - box[2][1])
         width = abs(box[0][0] - box[2][0])
 
-        # ɸѡ��Щ̫ϸ�ľ��Σ����±��
+
         if(height > width * 1.2):
             continue
 
@@ -82,17 +76,17 @@ def findTextRegion(img):
 
 
 def detect(img):
-    # 1.  ת���ɻҶ�ͼ
+
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # 2. ��̬ѧ�任��Ԥ������õ����Բ��Ҿ��ε�ͼƬ
+
     dilation = preprocess(gray)
 
-    # 3. ���Һ�ɸѡ��������
+
     region = findTextRegion(dilation)
     num_region = len(region)
 
-    # 4. �����߻�����Щ�ҵ�������,������ǵ�ƽ����
+
     num = 0
     for box in region:
         cv2.drawContours(img, [box], 0, (0, 255, 0), 2)
@@ -105,7 +99,7 @@ def detect(img):
     average_corner = num/num_region
     
 
-    # 5. ���ݽǵ���Ŀ��ɸѡ�����п��ܵ���������
+
     i = 1
     for box in region:
         x1 = box[0][0]
@@ -120,7 +114,7 @@ def detect(img):
     cv2.namedWindow("img", cv2.WINDOW_NORMAL)
     cv2.imshow("img", img)
 
-    # ��������ͼƬ
+
     cv2.imwrite("contours.png", img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
@@ -136,7 +130,7 @@ def num_corner(gray):
 
 
 if __name__ == '__main__':
-    # ��ȡ�ļ�
+
     #imagePath = sys.argv[1]
     img = cv2.imread('D:/text7.jpg')
     detect(img)
